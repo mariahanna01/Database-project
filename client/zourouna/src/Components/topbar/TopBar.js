2d1b566e12c6213608038ebf9e8c229a1fac3ab3
KarenEstephan <estephankaren@yahoo.com>
2021-11-09 20:13:35 +0200

    hi

---
+++ client/zourouna/src/Components/topbar/TopBar.js
@@ -0,0 +1,43 @@
+
+import React from'react'
+import "./topbar.css"
+
+
+export default function TopBar(){
+    return(
+        <div className= 'top'>
+            <div className ="topIcon">
+            <i className="fab fa-facebook-f"></i>
+            </div>
+            <div className="topIcon">
+            <i className="fab fa-instagram"></i>
+            </div>
+            <div className="topIcon">
+            <i className="fas fa-at"></i>
+            </div>
+            <div className="topCenter">
+                <ul className="topList">
+                <li className="topListItem">HOME</li>
+                    <li className="topListItem">ABOUT</li>
+                    <li className="topListItem">CONTACT</li>
+                    <li className="topListItem">WRITE</li>
+                    <li className="topListItem">LOGOUT</li>
+
+                </ul>
+            </div>
+
+            <div className="topRight">
+                <img 
+                className="topImg"
+                src="http://www.lebanoninapicture.com/Prv/Images/Pages/Page_85264/deeply-traditional-lebanese-village-of-baskinta-2-13-2017-7-11-13-pm-l.jpg" 
+                alt="" 
+                />
+                <i className="topSearchIcon fas fa-search-location"></i>
+
+                
+            </div>
+
+        </div>
+
+    )
+}
