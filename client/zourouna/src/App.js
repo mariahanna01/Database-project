12e2348a988a49f911f58a4205cca0abce9e7343
mariahanna01 <77756079+mariahanna01@users.noreply.github.com>
2021-11-11 22:14:53 +0200

    Update App.js

--- client/zourouna/src/App.js
+++ client/zourouna/src/App.js
@@ -3,14 +3,6 @@ import TopBar from './Components/topbar/TopBar';
 import Header from './Components/header/Header'
 import Sidebar from './Components/sidebar/Sidebar';
 
-<<<<<<< HEAD
-import Home from './Components/pages/home/Home';
-import TopBar from './Components/TopBar/TopBar';
-import Header from './Components/header/Header';
-import Sidebar from './Components/sidebar/Sidebar'
-=======
->>>>>>> d75ece50605bb3e782861f31759d35e8f53d7cf6
-
 function App() {
   return (
     <div className="App">
